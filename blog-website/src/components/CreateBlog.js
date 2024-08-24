81ac1dfa4051fc8837b337a0eecef38e58a6d29b
mitang0789 <hindochamitang@gmail.com>
2024-08-25 00:36:04 +0530

    v10
    v10

--- blog-website/src/components/CreateBlog.js
+++ blog-website/src/components/CreateBlog.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
 import "./CreateBlog.css";
 
 function CreateBlog() {
@@ -50,13 +52,14 @@ function CreateBlog() {
         </div>
         <div className="form-group">
           <label htmlFor="body">Blog Content</label>
-          <textarea
+          <ReactQuill
             id="body"
             value={body}
-            onChange={(e) => setBody(e.target.value)}
-            rows="10"
-            required
-          ></textarea>
+            onChange={setBody}
+            modules={CreateBlog.modules}
+            formats={CreateBlog.formats}
+            theme="snow"
+          />
         </div>
         <div className="form-group">
           <label htmlFor="tags">Tags (comma separated)</label>
@@ -75,5 +78,29 @@ function CreateBlog() {
     </div>
   );
 }
+
+CreateBlog.modules = {
+  toolbar: [
+    [{ header: "1" }, { header: "2" }, { font: [] }],
+    [{ list: "ordered" }, { list: "bullet" }],
+    ["bold", "italic", "underline"],
+    [{ color: [] }, { background: [] }],
+    ["link"],
+    ["clean"],
+  ],
+};
+
+CreateBlog.formats = [
+  "header",
+  "font",
+  "list",
+  "bullet",
+  "bold",
+  "italic",
+  "underline",
+  "color",
+  "background",
+  "link",
+];
 
 export default CreateBlog;
